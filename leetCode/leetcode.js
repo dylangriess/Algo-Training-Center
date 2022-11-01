89c03486893850d8ddf821ed7ba1869c150fa488
dylangriess <dylangriess@gmail.com>
2022-11-01 12:07:38 -0600

    parentheses algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -41,3 +41,30 @@ var twoSum = function (nums, target) {
 var isPalindrome = function (x) {
   return x == x.toString().split("").reverse().join("");
 };
+
+//20. Valid Parentheses
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//An input string is valid if:
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+//Every close bracket has a corresponding open bracket of the same type.
+var isValid = function (s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i += 1) {
+    const top = stack[stack.length - 1];
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      stack.push(s[i]);
+    } else if (s[i] === ")" && top === "(" && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === "]" && top === "[" && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === "}" && top === "{" && stack.length !== 0) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
