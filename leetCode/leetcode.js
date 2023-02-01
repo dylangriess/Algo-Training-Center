ce7025f9d5ea3963539f5545c571c686698049ac
dylangriess <dylangriess@gmail.com>
2023-02-01 15:34:45 -0700

    Introduce Word Pattern algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -135,3 +135,7 @@ var searchInsert = function (nums, target) {
   }
   return nums.length;
 };
+
+// 290. Word Pattern
+//Given a pattern and a string s, find if s follows the same pattern.
+// Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
