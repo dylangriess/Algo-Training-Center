bf34e5be277e317ba685b7f588b844e7ab217ec5
dylangriess <dylangriess@gmail.com>
2023-01-31 18:13:02 -0700

    first bad version algo complete

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -87,7 +87,6 @@ var helper = function (root, res) {
 // 704. Binary Search
 // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 // You must write an algorithm with O(log n) runtime complexity.
-
 var search = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
@@ -106,3 +105,21 @@ var search = function (nums, target) {
 // You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 // Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
 // You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let left = 1;
+    let right = n;
+
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (isBadVersion(mid)) right = mid;
+      else left = mid + 1;
+    }
+
+    return left;
+  };
+};
