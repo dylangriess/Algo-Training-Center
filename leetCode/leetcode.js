9be9ce851692899aee29c0f4eac7f26cd4a84fdf
dylangriess <dylangriess@gmail.com>
2023-01-31 18:13:54 -0700

    search insert position algo introduction

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -123,3 +123,7 @@ var solution = function (isBadVersion) {
     return left;
   };
 };
+
+// 35. Search Insert Position
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
