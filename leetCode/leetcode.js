716a34dceff53803bc1bd6e111ec449d6968b64c
dylangriess <dylangriess@gmail.com>
2023-02-01 15:36:23 -0700

    complete Word Pattern algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -139,3 +139,22 @@ var searchInsert = function (nums, target) {
 // 290. Word Pattern
 //Given a pattern and a string s, find if s follows the same pattern.
 // Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+var wordPattern = function (pattern, s) {
+  const words = s.split(" ");
+  if (pattern.length !== words.length) {
+    return false;
+  }
+  const charToWord = new Map();
+  const wordToChar = new Map();
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern[i];
+    const word = words[i];
+    if (!charToWord.has(char) && !wordToChar.has(word)) {
+      charToWord.set(char, word);
+      wordToChar.set(word, char);
+    } else if (charToWord.get(char) !== word || wordToChar.get(word) !== char) {
+      return false;
+    }
+  }
+  return true;
+};
