13bc55b2d7be9d83f64549899ae9e36246ab5d17
dylangriess <dylangriess@gmail.com>
2023-02-03 18:37:26 -0700

    complete Roman to Integer algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -189,3 +189,24 @@ var canArrange = function (arr, k) {
 //For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
 //Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
 //Given a roman numeral, convert it to an integer.
+
+var romanToInt = function (s) {
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let sum = map[s[s.length - 1]];
+  for (let i = s.length - 2; i >= 0; i--) {
+    if (map[s[i]] < map[s[i + 1]]) {
+      sum -= map[s[i]];
+    } else {
+      sum += map[s[i]];
+    }
+  }
+  return sum;
+};
