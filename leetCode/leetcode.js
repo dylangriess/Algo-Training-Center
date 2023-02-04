17345c9ed6fd134dccc8a613631ba449e42a0a24
dylangriess <dylangriess@gmail.com>
2023-02-03 18:16:54 -0700

    introduce Roman to Integer algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -183,3 +183,9 @@ var canArrange = function (arr, k) {
   }
   return true;
 };
+
+//13. Roman to Integer
+//Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+//For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+//Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+//Given a roman numeral, convert it to an integer.
