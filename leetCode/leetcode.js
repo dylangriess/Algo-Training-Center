e408084f0386f68721530df2a0669d7173ae0a7d
dylangriess <dylangriess@gmail.com>
2023-02-07 19:17:44 -0700

    introduce Sqrt algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -227,3 +227,7 @@ var removeElement = function (nums, val) {
 
   return k;
 };
+
+//69. Sqrt(x)
+//Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+//You must not use any built-in exponent function or operator.
