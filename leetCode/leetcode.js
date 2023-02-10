54c79f47702729fe85fdd3b2ace49fb6a1130af0
dylangriess <dylangriess@gmail.com>
2023-02-09 18:19:52 -0700

    introduce Pascal's Triangle algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -261,3 +261,7 @@ var merge = function (nums1, m, nums2, n) {
     k--;
   }
 };
+
+//118. Pascal's Triangle
+//Given an integer numRows, return the first numRows of Pascal's triangle.
+//In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
