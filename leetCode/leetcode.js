f90531641d82f4817b582c1cc81feac4198bdcec
dylangriess <dylangriess@gmail.com>
2023-02-16 17:18:30 -0700

    introduce Combine Two tables algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -316,3 +316,7 @@ function singleNumber(nums) {
   }
   return result;
 }
+
+//175. Combine Two Tables
+//Write an SQL query to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.
+//Return the result table in any order.
