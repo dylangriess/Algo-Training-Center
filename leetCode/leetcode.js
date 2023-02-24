eff0a467a85adca57789311d7fcea5eda5108761
dylangriess <dylangriess@gmail.com>
2023-02-23 19:06:15 -0700

    introduce Guess Number Higher Or Lower algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -339,3 +339,13 @@ function singleNumber(nums) {
 //Given a text file file.txt, print just the 10th line of the file.
 
 // sed -n '10p' file.txt | head -1
+
+//374. Guess Number Higher or Lower
+//We are playing the Guess Game. The game is as follows:
+//I pick a number from 1 to n. You have to guess which number I picked.
+//Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+//You call a pre-defined API int guess(int num), which returns three possible results:
+//-1: Your guess is higher than the number I picked (i.e. num > pick).
+//1: Your guess is lower than the number I picked (i.e. num < pick).
+//0: your guess is equal to the number I picked (i.e. num == pick).
+//Return the number that I picked.
