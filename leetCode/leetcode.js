43b2de222347df1a1caeda256ae7de97476dc06a
dylangriess <dylangriess@gmail.com>
2023-02-27 20:58:44 -0700

    complete Big Countries algo

--- leetCode/leetcode.js
+++ leetCode/leetcode.js
@@ -393,3 +393,7 @@ var fib = function (n) {
 //it has a population of at least twenty-five million (i.e., 25000000).
 //Write an SQL query to report the name, population, and area of the big countries.
 //Return the result table in any order.
+
+//SELECT name, population, area
+//FROM World
+//WHERE area >= 3000000 OR population >= 25000000;
